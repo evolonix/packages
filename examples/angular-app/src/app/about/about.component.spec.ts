82e4d57d03f7cf3d67d2f85dd84e52ee34917bf8
Jason Ruesch <jruesch@degreed.com>
2024-05-19 19:53:47 -0400

    test(repo): fix tests for CI

--- examples/angular-app/src/app/about/about.component.spec.ts
+++ examples/angular-app/src/app/about/about.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AboutComponent } from './about.component';
 
 describe('AboutComponent', () => {
@@ -7,7 +8,7 @@ describe('AboutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AboutComponent],
+      imports: [AboutComponent, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AboutComponent);
